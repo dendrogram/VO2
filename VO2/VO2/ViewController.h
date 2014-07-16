0a03f06c9313aa88853ea360fbd12a3262d9f6a7
jonhowell <j.a.howell@mmu.ac.uk>
2014-07-16 09:51:51 +0100

    adding vars and refs to vc.h

--- VO2/VO2/ViewController.h
+++ VO2/VO2/ViewController.h
@@ -11,30 +11,71 @@
 
 @interface ViewController : UIViewController <MFMailComposeViewControllerDelegate, UITextFieldDelegate>
 {
-    NSDate        *startDate;
+    // for file manager
+    NSFileManager * fileMgr;
+    NSString      * homeDir;
+    NSString      * filename;
+    NSString      * filepath;
     
-    NSFileManager *fileMgr;
-    NSString      *homeDir;
-    NSString      *filename;
-    NSString      *filepath;
+    // for calculations and functions
+    NSString      * labLocation;
+    
+    NSDate        * startDate;
+    NSDate        * testDate;
+    
+    float subWt;
+    float subHt;
+    float labPressure_mmHg;
+    float labPressure_mBar;
+    float labTemp;
+    float labHumidity;
+    float VEATPS;
+    float VESTPD;
+    float VO2;
+    float VCO2;
+    float RER;
+    float sampTime;
+    float FECO2;
+    float FEO2;
+    float corrFactor;
 }
 
 //file ops stuff
-@property(nonatomic,retain) NSFileManager *fileMgr;
-@property(nonatomic,retain) NSString *homeDir;
-@property(nonatomic,retain) NSString *filename;
-@property(nonatomic,retain) NSString *filepath;
+@property(nonatomic,retain) NSFileManager * fileMgr;
+@property(nonatomic,retain) NSString      * homeDir;
+@property(nonatomic,retain) NSString      * filename;
+@property(nonatomic,retain) NSString      * filepath;
+
+
 
 -(NSString *) GetDocumentDirectory;
 -(NSString *) setFilename;
 -(void) WriteToStringFile:(NSMutableString *)textToWrite;
 
-@property (nonatomic, copy) NSDate *startDate;
+//dates
+@property (nonatomic, copy) NSDate * startDate;
+@property (nonatomic, copy) NSDate * testDate;
 
 //email stuff
-@property (nonatomic, strong) IBOutlet UITextField *subjectNameTxt;
+@property (nonatomic, strong) IBOutlet UITextField * subjectNameTxt;
+@property (nonatomic, strong) IBOutlet UITextField * testerNameTxt;
+@property (nonatomic, strong) IBOutlet UITextField * labLocationTxt;
+
+
+// for calculations and functions
+
 
 // button press to send the mail
 -(IBAction)sendEmail:(id)sender;
+
+-(IBAction)convertPressure_to_mmHg:(id)sender;
+-(IBAction)convertPressure_to_mBar:(id)sender;
+-(IBAction)calcVESPTD:(id)sender;
+-(IBAction)calcVO2:(id)sender;
+-(IBAction)calcVCO2:(id)sender;
+-(IBAction)calcVO2Kg:(id)sender;
+-(IBAction)calcVCO2Kg:(id)sender;
+-(IBAction)calcRER:(id)sender;
+
 
 @end
